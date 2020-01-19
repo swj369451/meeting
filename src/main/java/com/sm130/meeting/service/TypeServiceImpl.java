cbc6d049b15b2c0fea29810011dcd9e884560e51
swj369451 <swj369451@163.com>
2020-01-19 17:36:14 +0800

    完成分类的功能

---
+++ src/main/java/com/sm130/meeting/service/TypeServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sm130.meeting.service;
+
+import com.sm130.meeting.NotFoundException;
+import com.sm130.meeting.dao.TypeRepository;
+import com.sm130.meeting.po.Type;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class TypeServiceImpl implements TypeService {
+
+    @Autowired
+    private TypeRepository typeRepository;
+
+    @Transactional
+    @Override
+    public Type saveType(Type type) {
+        return typeRepository.save(type);
+    }
+
+    @Transactional
+    @Override
+    public Type getType(Long id) {
+        return typeRepository.getOne(id);
+    }
+
+    @Override
+    public Type getTypeByName(String name) {
+        return typeRepository.findByName(name);
+    }
+
+    @Transactional
+    @Override
+    public Page<Type> listType(Pageable pageable) {
+        return typeRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Type> listType() {
+        return typeRepository.findAll();
+    }
+
+
+    @Override
+    public List<Type> listTypeTop(Integer size) {
+//        Sort sort = new Sort(Sort.Direction.DESC,"meetings.size");
+//        Pageable pageable = new PageRequest(0,size,sort);
+//        return typeRepository.findTop(pageable);
+        return null;
+    }
+
+
+    @Transactional
+    @Override
+    public Type updateType(Long id, Type type) {
+        Type t = typeRepository.getOne(id);
+        if (t == null) {
+            throw new NotFoundException("不存在该类型");
+        }
+        BeanUtils.copyProperties(type,t);
+        return typeRepository.save(t);
+    }
+
+
+
+    @Transactional
+    @Override
+    public void deleteType(Long id) {
+        typeRepository.deleteById(id);
+    }
+}
